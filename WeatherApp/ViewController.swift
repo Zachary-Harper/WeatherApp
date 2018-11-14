3ed6c407a9ed416e469deebfb20af1abb5eaae88
Zachary Harper <zacharyharper@MacBook-Pro-8.local>
2018-11-14 12:35:12 -0600

    pod install

---
+++ WeatherApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WeatherApp
+//
+//  Created by Zachary Harper on 11/14/18.
+//  Copyright © 2018 Zachary Harper. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
